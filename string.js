08560e70fc618015560814b52a91a44f5152c8e3
Manish7113 <kumawatji7113@gmail.com>
2024-06-04 14:33:59 +0530

    init

---
+++ string.js
@@ -0,0 +1,56 @@
+// Here is some string methods 
+
+
+// creating a string 
+
+const str = `https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/anchors`
+const otherString = new String('Hello other developer')
+
+// -------------- length method ------------------- 
+
+// console.log(str.length)
+// const stringLength =  str.length;
+// console.log(stringLength , 'string length ')
+
+ 
+// -------------- toLowerCase()  // toUpperCase() ------------------------
+
+// console.log(str.toLocaleLowerCase())
+// console.log(str.toUpperCase())
+
+// toWellFormed ?????????????????
+
+
+// ---------------trim() // trimStart() // trimEnd() ----------------------------
+
+// console.log(str.trim() , 'trimmed ')
+// console.log(str, 'non trimmed  ')
+
+
+// console.log(str.trimStart(), 'trimmed from start ')
+// console.log(str, 'non trimmed  ')
+
+// 
+// console.log(str.trimEnd(), 'trimmed from end ')
+// console.log(str, 'non trimmed  ')
+
+
+
+// ------------------------- charAt() ---------------------
+
+// console.log(otherString.charAt(21) , 'character at 0')
+// console.log(otherString.length , 'length  at 0')
+
+
+// anchore method dont have to use now  it is also removed form many browsers but some browser supports it 
+
+// -----------------------at method --------------------------
+
+// console.log(otherString.at(-3) , 'data at the ') // return the character from the last 
+// console.log(otherString.at(4) , 'data at the ') 
+
+
+// ------------------------------- charCodeAt() ----------------------
+console.log(str.charCodeAt(2), 'testttttttttttttttt')
+
+
