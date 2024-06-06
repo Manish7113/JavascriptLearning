3789ee8bf225d3134f469db31049809107d9db8e
Manish7113 <kumawatji7113@gmail.com>
2024-06-06 16:14:27 +0530

    updates

---
+++ date.js
@@ -0,0 +1,10 @@
+const myDate = new Date();
+// console.log(myDate.toDateString() , 'my date') //Thu Jun 06 2024 
+// console.log(myDate.toISOString() , 'my date') // 2024-06-06T09:54:03.314Z 
+// console.log(myDate.toJSON() , 'my date') // 2024-06-06T09:54:48.356Z
+// console.log(myDate.toLocaleDateString() , 'my date') //6/6/2024
+// console.log(myDate.toLocaleTimeString() , 'my date') //3:26:26
+// console.log(myDate.toUTCString() , 'my date') //Thu, 06 Jun 2024 09:56:55 GMT
+
+const myDob = new Date(2002, 10, 17);
+console.log(myDob.toDateString())
---
+++ math.js
@@ -0,0 +1,38 @@
+const score = 100;
+const balance = new Number(100000000)
+// console.log(score)
+
+
+
+// console.log(balance.toExponential(2), 'exponential ')
+// console.log(balance.toFixed(1) , 'toFixed')
+// console.log(balance.toLocaleString('en-In'), 'to local string ')
+// console.log(balance.valueOf(), 'value of ')
+
+// tostring to apply the string property on it just like length 
+
+
+// -----------------------------Math ------------------
+const mathNumber = 34.5677;
+// console.log(Math.abs(mathNumber)) // convert neagetive to positive -- (-3)
+// console.log(Math.round(mathNumber)) // round of the floting point -- 34.5677
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.3))
+// console.log(Math.sqrt(4))
+// const myArray = [34, 54, 12, 43]
+// console.log(myArray ,'my array ')
+// console.log(Math.min(...myArray))
+// console.log(Math.max(4, 35, 65, 90))
+
+
+// getting minimum number between the range and that number becomes an random number 
+
+const min = 12;
+const max = 14;
+const randomNumber  = Math.floor((Math.random() * (max - min +1) + min))
+console.log(randomNumber , 'random number')
+
+
+
+
+
