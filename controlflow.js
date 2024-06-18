5cdb414fba360f9e160aa03c343522a3f7d98aa0
Manish7113 <kumawatji7113@gmail.com>
2024-06-18 12:11:14 +0530

    updates

---
+++ controlflow.js
@@ -0,0 +1,78 @@
+// const score = 68;
+// switch(true)
+// {
+//     case (score > 50 && score < 60): 
+//     console.log('More than 50');
+//     break;
+//     case (score > 60 && score < 70): 
+//     console.log('More than 60');
+//     break;
+//     case (score > 70 && score < 80): 
+//     console.log('More than 70');
+//     break;
+//     case (score > 80 && score < 60): 
+//     console.log('More than 80');
+//     break;
+//     default : console.log('more than 90')
+//     break;
+// }
+
+
+///////////////////////////////
+
+
+// const score = 68;
+
+// switch(true) {
+//     case (score > 50 && score < 60): 
+//         console.log('More than 50');
+//         break;
+//     case (score >= 60 && score < 70): 
+//         console.log('More than 60');
+//         break;
+//     case (score >= 70 && score < 80): 
+//         console.log('More than 70');
+//         break;
+//     case (score >= 80 && score < 90): 
+//         console.log('More than 80');
+//         break;
+//     default:
+//         console.log('Score is more than 90 or less than 50');
+//         break;
+// }
+
+
+
+// runs all the condition after statisfying but only default is not running 
+// const number  = 50;
+
+// switch (number) {
+//     case 40:
+//         console.log(40 , '400000')
+        
+        
+//     case 50:
+//         console.log(50 , '500000')
+        
+        
+//     case 60:
+//         console.log(60 , '600000')
+        
+        
+//     case 70:
+//         console.log(70 , '400000')
+        
+        
+//     case 80:
+//         console.log(80 , '400000')
+        
+        
+
+//     default:
+//         console.log('Hellow')
+//         break;
+// }
+
+
+// const name = 'undefined' ?? "Manish "
+// console.log(name , 'name')
